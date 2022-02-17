287587409e6610057f2c399e931e80be3e44ffca
evasga <evasga98@gmail.com>
2022-02-17 10:28:34 +0100

    adding implicit wait

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,8 @@
 require 'selenium-webdriver'
 Before do |scenario|
   @browser = Selenium::WebDriver.for :chrome
+  @browser.manage.timeouts.implicit_wait = 10
+
+  @browser.navigate.to 'https://www.amazon.com/?language=en_US'
   @browser.manage.window.maximize
-  @browser.navigate.to 'http://www.amazon.com'
 end
